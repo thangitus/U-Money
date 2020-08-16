518f18b3faab6ded4e29d8bcc89bc86673e76fe0
thangitus <354895123578951nvt>
2020-08-16 20:34:16 +0700

    Custom button add

--- app/src/main/java/com/itus/u_money/mvp/view/activity/MainActivity.java
+++ app/src/main/java/com/itus/u_money/mvp/view/activity/MainActivity.java
@@ -1,37 +1,54 @@
 package com.itus.u_money.mvp.view.activity;
 
+import android.content.res.ColorStateList;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.facebook.stetho.Stetho;
+import com.facebook.stetho.inspector.elements.android.ActivityTracker;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.itus.u_money.R;
+import com.itus.u_money.databinding.ActivityMainBinding;
 import com.itus.u_money.mvp.view.fragment.PlanFragment;
 import com.itus.u_money.mvp.view.fragment.ReportFragment;
 import com.itus.u_money.mvp.view.fragment.UserFragment;
 import com.itus.u_money.mvp.view.fragment.WalletFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+   ActivityMainBinding binding;
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_main);
+      binding = ActivityMainBinding.inflate(getLayoutInflater());
+      setContentView(binding.getRoot());
       loadFragment(new WalletFragment());
-
+      binding.frameContainer.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+            binding.motionLayout.transitionToStart();
+         }
+      });
       Stetho.initializeWithDefaults(this);
 
-      BottomNavigationView navigationView = findViewById(R.id.navigation);
-      navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+      binding.navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
          @Override
          public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             Fragment fragment = null;
             switch (item.getItemId()) {
+               case R.id.navigation_null:
+                  revertMotion();
+                  break;
                case R.id.navigation_wallet:
                   fragment = new WalletFragment();
                   break;
@@ -46,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
                   break;
             }
             if (fragment != null) {
+               binding.motionLayout.transitionToStart();
                loadFragment(fragment);
                return true;
             } else
@@ -54,9 +72,16 @@ public class MainActivity extends AppCompatActivity {
       });
    }
 
+   public void revertMotion() {
+      if (binding.motionLayout.getProgress() == 1)
+         binding.motionLayout.transitionToStart();
+      else
+         binding.motionLayout.transitionToEnd();
+   }
    private void loadFragment(Fragment fragment) {
       FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
       fragmentTransaction.replace(R.id.frame_container, fragment);
       fragmentTransaction.commit();
    }
+
 }
