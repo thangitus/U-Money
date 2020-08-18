eb91eeb3f8ae54fb3e05accb4236e3117e33b5df
thangitus <354895123578951nvt>
2020-08-18 09:26:37 +0700

    Add transacion UI

--- app/src/main/java/com/itus/u_money/mvp/view/activity/AddTransactionActivity.java
+++ app/src/main/java/com/itus/u_money/mvp/view/activity/AddTransactionActivity.java
@@ -2,12 +2,17 @@ package com.itus.u_money.mvp.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 
 import com.itus.u_money.R;
 import com.itus.u_money.databinding.ActivityAddTransactionBinding;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 import java.util.Objects;
 
 public class AddTransactionActivity extends AppCompatActivity {
@@ -30,4 +35,24 @@ public class AddTransactionActivity extends AppCompatActivity {
    public void saveTransaction(View view) {
 
    }
+   public void selectGroup(View view) {}
+   public void selectDate(View view) {
+      final Calendar myCalendar = Calendar.getInstance();
+      int day = myCalendar.get(Calendar.DAY_OF_MONTH);
+      int month = myCalendar.get(Calendar.MONTH);
+      int year = myCalendar.get(Calendar.YEAR);
+      DatePickerDialog datePickerDialog = new DatePickerDialog(AddTransactionActivity.this, R.style.DialogTheme, (datePicker, i, i1, i2) -> {
+         myCalendar.set(Calendar.YEAR, i);
+         myCalendar.set(Calendar.MONTH, i1);
+         myCalendar.set(Calendar.DAY_OF_MONTH, i2);
+         String myFormat = "dd/MM/yyyy";
+         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+         binding.edtDate.setText(sdf.format(myCalendar.getTime()));
+         binding.edtDate.invalidate();
+         binding.edtDate.requestLayout();
+      }, year, month, day);
+      datePickerDialog.show();
+   }
+   public void selectEvent(View view) {}
+   public void selectImage(View view) {}
 }
