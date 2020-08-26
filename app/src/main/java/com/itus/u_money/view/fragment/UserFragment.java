c7e0cd0cf404a84755af004c8d99554d423fee4a
thangitus <thang.itus@gmail.com>
2020-08-26 11:07:37 +0700

    User Fragment

--- app/src/main/java/com/itus/u_money/view/fragment/UserFragment.java
+++ app/src/main/java/com/itus/u_money/view/fragment/UserFragment.java
@@ -1,5 +1,6 @@
 package com.itus.u_money.view.fragment;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,14 +11,29 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.itus.u_money.R;
+import com.itus.u_money.databinding.FragmentUserBinding;
 
 public class UserFragment extends Fragment {
+   FragmentUserBinding binding;
 
+   public UserFragment() {
+   }
    @Nullable
    @Override
    public View onCreateView(
            @NonNull LayoutInflater inflater,
            @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-      return inflater.inflate(R.layout.fragment_user, container, false);
+      binding = FragmentUserBinding.inflate(getLayoutInflater());
+      return binding.getRoot();
+   }
+   @Override
+   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+      super.onViewCreated(view, savedInstanceState);
+      SharedPreferences pref = getContext().getSharedPreferences("UMoney", 0); // 0 - for private mode
+      String fullName = pref.getString("FullName", "");
+      long curAmount = pref.getLong("CurAmount", 0);
+      binding.edtFullName.setText(fullName);
+      binding.currentMoney.setText(curAmount + " VND");
+      binding.edtBudget.setText(" ");
    }
 }
