69d0ae772551c2120e006cdaca9bbd405c2e9c3c
thangitus <thang.itus@gmail.com>
2020-08-26 15:47:38 +0700

    Setup Kotlin

---
+++ app/src/main/java/com/itus/u_money/view/fragment/UserFragment.kt
@@ -0,0 +1,36 @@
+package com.itus.u_money.view.fragment
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.itus.u_money.databinding.FragmentUserBinding
+
+class UserFragment : Fragment() {
+    private var TAG="UserFragment"
+
+    var binding: FragmentUserBinding? = null
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentUserBinding.inflate(layoutInflater)
+        return binding!!.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: ");
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val pref = context!!.getSharedPreferences("UMoney", 0) // 0 - for private mode
+        val fullName = pref.getString("FullName", "")
+        val curAmount = pref.getLong("CurAmount", 0)
+        binding!!.edtFullName.setText(fullName)
+        binding!!.currentMoney.setText("$curAmount VND")
+        binding!!.edtBudget.setText(" ")
+    }
+}
